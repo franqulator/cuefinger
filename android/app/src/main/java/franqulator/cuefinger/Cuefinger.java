6d2c80585def5e0402792ea1a2f68ef6a437990d
franqulator <mail@fbrempel.net>
2024-06-02 23:25:47 +0200

    move to own repository

--- android/app/src/main/java/franqulator/cuefinger/Cuefinger.java
+++
@@ -1,9 +0,0 @@
-package franqulator.cuefinger;
-
-import org.libsdl.app.SDLActivity;
-
-/**
- * A sample wrapper class that just calls SDLActivity
- */
-
-public class Cuefinger extends SDLActivity { }
